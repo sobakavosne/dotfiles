be84202bd6f83578458b25c1e78a9d281af04fcc
Λ <sobakavosne@gmail.com>
2024-05-08 01:12:11 +0200

    add BT status script

---
+++ bluetooth_status.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if Bluetooth service is active
+if systemctl is-active --quiet bluetooth; then
+	    echo "BT:On |" # Bluetooth is active
+    else
+	        echo "BT:Off |" # Bluetooth is not active
+fi
+
+
