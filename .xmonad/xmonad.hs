9e5023627475f0b912a3949f607501f9bf0b2d7e
Λ <sobakavosne@gmail.com>
2024-06-27 16:27:45 +0200

    remove unused & add `Send window` command

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -56,72 +56,26 @@ myFont :: String
 myFont =
   "xft:SauceCodePro Nerd Font Mono:regular:size=9:antialias=true:hinting=true"
 
--- modMask lets you specify which modkey you want to use. The default
--- is mod1Mask ("left alt").  You may also consider using mod3Mask
--- ("right alt"), which does not conflict with emacs keybindings. The
--- "windows key" is usually mod4Mask.
+-- mod1Mask - left alt
+-- mod3Mask - right alt
+-- mod4Mask - windows key
 --
 myModMask = mod1Mask
 
--- The default number of workspaces (virtual screens) and their names.
--- By default we use numeric strings, but any string may be used as a
--- workspace name. The number of workspaces is determined by the length
--- of this list.
---
--- A tagging example:
---
--- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
---
-myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+myWorkspaces = ["search", "web", "code", "teminal", "irc"] ++ map show [6 .. 9]
 
 myNormalBorderColor = "#282c34"
 
 myFocusedBorderColor = "#46d9ff"
 
-colorScheme = "solarized-light"
-
 colorBack = "#fdf6e3"
-
-colorFore = "#657b83"
-
-color01 = "#073642"
-
-color02 = "#dc322f"
-
-color03 = "#859900"
-
-color04 = "#b58900"
-
-color05 = "#268bd2"
-
-color06 = "#d33682"
-
-color07 = "#2aa198"
 
 color08 = "#eee8d5"
 
-color09 = "#002b36"
-
-color10 = "#cb4b16"
-
-color11 = "#586e75"
-
-color12 = "#657b83"
-
-color13 = "#839496"
-
-color14 = "#fdf6e3" -- Was color16 but too light for use in panel.
-
 color15 = "#93a1a1"
 
-color16 = "#6c71c4" -- Was color14 but needed a dark color in this spot.
+color16 = "#6c71c4"
 
-colorTrayer :: String
-colorTrayer = "--tint 0xfdf6e3"
-
-------------------------------------------------------------------------
--- Key bindings. Add, modify or remove key bindings here.
---
 myKeys conf@XConfig {XMonad.modMask = modm} =
   M.fromList $
     -- launch a terminal
@@ -136,24 +90,12 @@ myKeys conf@XConfig {XMonad.modMask = modm} =
   , ((modm, xK_space), sendMessage NextLayout)
     --  Reset the layouts on the current workspace to default
   , ((modm .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf)
-    -- Resize viewed windows to the correct size
-  , ((modm, xK_n), refresh)
     -- Move focus to the next window
   , ((modm, xK_Tab), windows W.focusDown)
-    -- Move focus to the next window
-  , ((modm, xK_j), windows W.focusDown)
-    -- Move focus to the previous window
-  , ((modm, xK_k), windows W.focusUp)
-    -- Move focus to the master window
-  , ((modm, xK_m), windows W.focusMaster)
     -- Swap the focused window and the master window
   , ((modm, xK_Return), windows W.swapMaster)
-    -- Swap the focused window with the next window
-  , ((modm .|. shiftMask, xK_j), windows W.swapDown)
-    -- Swap the focused window with the previous window
-  , ((modm .|. shiftMask, xK_k), windows W.swapUp)
     -- Shrink the master area
-  , ((modm, xK_h), sendMessage Shrink)
+  , ((modm, xK_k), sendMessage Shrink)
     -- Expand the master area
   , ((modm, xK_l), sendMessage Expand)
     -- Push window back into tiling
@@ -166,7 +108,7 @@ myKeys conf@XConfig {XMonad.modMask = modm} =
     -- Use this binding with avoidStruts from Hooks.ManageDocks.
     -- See also the statusBar function from Hooks.DynamicLog.
     --
-    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
+  , ((modm, xK_b), sendMessage ToggleStruts)
     -- Quit xmonad
   , ((modm .|. shiftMask, xK_q), io exitSuccess)
     -- Restart xmonad
@@ -179,28 +121,13 @@ myKeys conf@XConfig {XMonad.modMask = modm} =
     -- Switch to the previous workspace
   , ((modm .|. controlMask, xK_Left), prevWS)
   ] ++
-    --
-    -- mod-[1..9], Switch to workspace N
-    -- mod-shift-[1..9], Move client to workspace N
-    --
-  [ ((m .|. modm, k), windows $ f i)
-  | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-  , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
-  ] ++
-    --
-    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
-    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
-    --
-  [ ((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-  | (key, sc) <- zip [xK_w, xK_e, xK_r] [0 ..]
-  , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
+    -- Send window to the workspace
+  [ ((modm .|. shiftMask, xK_Left), shiftToPrev >> prevWS) -- Send window to previous workspace
+  , ((modm .|. shiftMask, xK_Right), shiftToNext >> nextWS) -- Send window to next workspace
   ] ++
     -- super-space, Change keyboard layout
   [((mod4Mask, xK_space), spawn "~/.xmonad/toggle-layout.sh")]
 
-------------------------------------------------------------------------
--- Mouse bindings: default actions bound to mouse events
---
 myMouseBindings XConfig {XMonad.modMask = modm} =
   M.fromList
     -- mod-button1, Set the window to floating mode and move by dragging
@@ -214,27 +141,7 @@ myMouseBindings XConfig {XMonad.modMask = modm} =
     -- you may also bind events to the mouse scroll wheel (button4 and button5)
     ]
 
-------------------------------------------------------------------------
--- Layouts:
--- You can specify and transform your layouts by modifying these values.
--- If you change layout bindings be sure to use 'mod-shift-space' after
--- restarting (with 'mod-q') to reset your layout state to the new
--- defaults, as xmonad preserves your old layout settings by default.
---
--- The available layouts.  Note that each layout is separated by |||,
--- which denotes layout choice.
---
-myLayout = avoidStruts (tiled ||| Mirror tiled ||| Full)
-  where
-    -- default tiling algorithm partitions the screen into two panes
-    tiled = Tall nmaster delta ratio
-    -- The default number of windows in the master pane
-    nmaster = 1
-    -- Default proportion of screen occupied by master pane
-    ratio = 1 / 2
-    -- Percent of screen to increment by when resizing panes
-    delta = 3 / 100
-
+-- Layouts
 mySpacing ::
      Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
 mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
@@ -301,16 +208,6 @@ myManageHook =
     ]
 
 ------------------------------------------------------------------------
--- Event handling
--- * EwmhDesktops users should change this to ewmhDesktopsEventHook
---
--- Defines a custom handler function for X Events. The function should
--- return (All True) if the default handler is to be run afterwards. To
--- combine event hooks use mappend or mconcat from Data.Monoid.
---
-myEventHook = mempty
-
-------------------------------------------------------------------------
 -- Status bars and logging
 -- Perform an arbitrary action on each internal state change or X event.
 -- See the 'XMonad.Hooks.DynamicLog' extension for examples.
@@ -343,7 +240,6 @@ defaults =
       -- hooks, layouts
     , layoutHook = myLayoutHook
     , manageHook = myManageHook
-    , handleEventHook = myEventHook
     , logHook = myLogHook
     , startupHook = myStartupHook
     }
