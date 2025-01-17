51dc4113f3457c29d9fb6c9d4ab68caa501debc2
Λ <sobakavosne@protonmail.com>
2025-01-17 06:35:31 +0100

    fix(xmonad): fix `check_battery` script

--- xmonad/check_battery.sh
+++ xmonad/check_battery.sh
@@ -7,7 +7,9 @@ send_notification() {
     if [ ! -f "$NOTIFICATION_ID_FILE" ]; then
         echo "Sending notification"
         NOTIFICATION_ID=$(dunstify -u critical "Low Battery" "Battery level is ${BATTERY_LEVEL}%. Please plug in your charger." -p)
-        echo $NOTIFICATION_ID > $NOTIFICATION_ID_FILE
+        if [[ "$NOTIFICATION_ID" =~ ^[0-9]+$ ]]; then
+            echo $NOTIFICATION_ID > $NOTIFICATION_ID_FILE
+        fi
     fi
 }
 
@@ -15,20 +17,23 @@ dismiss_notification() {
     if [ -f "$NOTIFICATION_ID_FILE" ]; then
         echo "Dismissing notification"
         NOTIFICATION_ID=$(cat $NOTIFICATION_ID_FILE)
-        dunstify -C $NOTIFICATION_ID
-        rm $NOTIFICATION_ID_FILE
+        if [[ "$NOTIFICATION_ID" =~ ^[0-9]+$ ]]; then
+            dunstify -C "$NOTIFICATION_ID"
+        fi
+        rm -f $NOTIFICATION_ID_FILE
         notify-send -u low -c "battery" -i none "Dismiss" "Battery is now charging."
     fi
 }
 
 while true; do
-    BATTERY_STATUS=$(acpi -b | grep -o 'Discharging')
-    BATTERY_LEVEL=$(acpi -b | grep -P -o '[0-9]+(?=%)')
+    BATTERY_INFO=$(acpi -b)
+    BATTERY_STATUS=$(echo "$BATTERY_INFO" | grep -oP '(Discharging|Charging|Full)')
+    BATTERY_LEVEL=$(echo "$BATTERY_INFO" | grep -P -o '[0-9]+(?=%)')
 
     echo "Battery Status: $BATTERY_STATUS"
     echo "Battery Level: $BATTERY_LEVEL"
 
-    if [ "$BATTERY_STATUS" == "Discharging" ]; then
+    if [[ "$BATTERY_STATUS" == "Discharging" ]]; then
         if [ "$BATTERY_LEVEL" -le "$LOW_BATTERY_THRESHOLD" ]; then
             send_notification
         fi
